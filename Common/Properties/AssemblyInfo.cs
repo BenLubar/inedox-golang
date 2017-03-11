e1b998c4b4078cfe8d7aeb10b54470159ead714a
Ben Lubar <ben.lubar@gmail.com>
2017-03-11 16:50:25 -0600

    Initial commit

---
+++ Common/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Golang")]
+[assembly: AssemblyDescription("Contains operations helpful when building programs written in Google Go.")]
+
+[assembly: ComVisible(false)]
+[assembly: AssemblyCompany("Inedo, LLC")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
+[assembly: CLSCompliant(false)]
